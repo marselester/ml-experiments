a4b2f2e05e95f48876e9106772dbabe061a3504b
marselester <marselester@ya.ru>
2014-07-04 09:01:41 +0700

    Add intercept term adding to feature matrices in run.py

--- ml/run.py
+++ ml/run.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from .split import split_to_sets, split_feature_from_target
-from .norm import column_mean, column_std, feature_normalize
+from .norm import (column_mean, column_std, feature_normalize,
+                   add_column_of_ones_to_matrix)
 
 csvfile_path = 'data/concrete.csv'
 dataset = np.genfromtxt(csvfile_path, delimiter=',')
@@ -18,3 +19,8 @@ std = column_std(X_train)
 X_norm_train = feature_normalize(X_train, mean, std)
 X_norm_cv = feature_normalize(X_cv, mean, std)
 X_norm_test = feature_normalize(X_test, mean, std)
+
+# Add intercept term to feature matrices.
+X_norm_train = add_column_of_ones_to_matrix(X_norm_train)
+X_norm_cv = add_column_of_ones_to_matrix(X_norm_cv)
+X_norm_test = add_column_of_ones_to_matrix(X_norm_test)
